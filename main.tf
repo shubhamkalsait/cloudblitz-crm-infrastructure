202189fe70c62593846663c5eb7a48e4dfa031fe
skalsait <skalsait@dyl.com>
2024-02-18 13:05:47 +0530

    eks added

--- main.tf
+++ main.tf
@@ -2,7 +2,8 @@ provider "aws" {
     region = "us-east-1"
 }
 
-data "aws_iam_policy_document" "assume_role" {
+########### IAM ROLE FOR EKS ##############
+data "aws_iam_policy_document" "assume_role_eks" {
   statement {
     effect = "Allow"
 
@@ -15,23 +16,25 @@ data "aws_iam_policy_document" "assume_role" {
   }
 }
 
-resource "aws_iam_role" "example" {
+resource "aws_iam_role" "eks_role" {
   name               = "eks-cluster-example"
-  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+  assume_role_policy = data.aws_iam_policy_document.assume_role_eks.json
 }
 
 resource "aws_iam_role_policy_attachment" "example-AmazonEKSClusterPolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-  role       = aws_iam_role.example.name
+  role       = aws_iam_role.eks_role.name
 }
 
 # Optionally, enable Security Groups for Pods
 # Reference: https://docs.aws.amazon.com/eks/latest/userguide/security-groups-for-pods.html
 resource "aws_iam_role_policy_attachment" "example-AmazonEKSVPCResourceController" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
-  role       = aws_iam_role.example.name
+  role       = aws_iam_role.eks_role.name
 }
 
+
+########## EKS CLUSTER ############
 resource "aws_eks_cluster" "cluster_example" {
   name     = "cluster-example"
   role_arn = aws_iam_role.example.arn
@@ -48,3 +51,87 @@ resource "aws_eks_cluster" "cluster_example" {
   ]
 }
 
+
+########## IAM ROLE FOR NODE Groups#########
+resource "aws_iam_role" "node_role" {
+  name = "eks-node-group-example"
+
+  assume_role_policy = jsonencode({
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+    }]
+    Version = "2012-10-17"
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "example-AmazonEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.node_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "example-AmazonEKS_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.node_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "example-AmazonEC2ContainerRegistryReadOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.node_role.name
+}
+
+
+########### NODE GROUP ###########
+resource "aws_eks_node_group" "example" {
+  cluster_name    = aws_eks_cluster.cluster_example.name
+  node_group_name = "cluster_node_group"
+  node_role_arn   = aws_iam_role.node_role.arn
+  subnet_ids      = [var.subnet1_id, var.subnet2_id]
+
+  scaling_config {
+    desired_size = 2
+    max_size     = 2
+    min_size     = 2
+  }
+
+  update_config {
+    max_unavailable = 1
+  }
+
+  # Ensure that IAM Role permissions are created before and deleted after EKS Node Group handling.
+  # Otherwise, EKS will not be able to properly delete EC2 Instances and Elastic Network Interfaces.
+  depends_on = [
+    aws_iam_role_policy_attachment.example-AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.example-AmazonEKS_CNI_Policy,
+    aws_iam_role_policy_attachment.example-AmazonEC2ContainerRegistryReadOnly,
+    aws_eks_cluster.cluster_example
+  ]
+}
+
+
+########## RDS INSTANCE ############
+resource "aws_db_instance" "default" {
+  allocated_storage    = 20
+  db_name              = "mydb"
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t3.micro"
+  username             = "admin"
+  password             = "redhat"
+  parameter_group_name = "default.mysql5.7"
+  skip_final_snapshot  = true
+}
+
+
+########## S3 Bucket ###########
+resource "aws_s3_bucket" "example" {
+  bucket = "cloudblitz-infra"
+
+  tags = {
+    Name        = "cloudblitz-infra"
+    Environment = "dev"
+  }
+}
